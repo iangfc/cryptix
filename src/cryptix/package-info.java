1f7f12a6edddf47c5944963014ffd1f46bac3613
Iang <iang@iang.org>
2015-01-30 11:45:14 +0000

    intro

--- src/cryptix/package-info.java
+++ src/cryptix/package-info.java
@@ -1,8 +1,57 @@
 /**
+ * <p>This project includes crypto code that was produced for the
+ * 4th generation WebFunds project.  The intent is to put all
+ * of the crypto code into a separate library so it is a bit
+ * clearer what is done and what is available.</p>
  * 
- */
-/**
+ * <p>It is not recommended that you follow this style.  It is
+ * far better for your application to copy in the parts you need,
+ * customise and tune them, then carry on.  Crypto is integral
+ * to your application and isolating and extracting it
+ * into libraries can bite back.
+ * (Note that does not apply to WebFunds because we maintain
+ * both code bases in parallel and are at liberty to fold it
+ * all in again...)</p>
+ * 
+ * <p>As well as crypto code it includes a small amount of support
+ * code in cryptix.util.  That's unfortunate but essential to write
+ * the various testing and interactions needed.</p>
+ * 
+ * <h2> Permission to use </h2>
+ * <p>The original code in this library is licensed under a
+ * free-speech all-you-can-eat public-domain airdrop.</p>
+ * 
+ * <ul><li>You are free to speak this code any way you wish or need.</ul>
+ * <li>Attribution is nice but not necessary.</ul>
+ * <li>Nothing is warrantied, promised, guaranteed -- use at your
+ * own risk and your own liability.</li></ul>
+ * 
+ * <h2> Dependencies </h2>
+ * <p>There are no patented contents, to our knowledge, and we would
+ * remove those with a vengeance.  Patents are a blight on the Internet.</p>
+ * 
+ * <p>There is a small amount of code copied or inspired from BouncyCastle
+ * raw crypto library.
+ * This is mostly the interface style and digests.
+ * That code remains under their licence.</p>
+ * 
+ * <p>There are a few papers copied in here for convenience.
+ * These are the copyright of the authors, but they are the
+ * sort of paper that is meant to be copied around.  If in
+ * doubt, check the home site by googling the title.</p>
+ * 
+ * <p>The library is entirely independent of all other libraries,
+ * it requires no other code or library to be downloaded.
+ * As it should be.</p>
+ * 
+ * <h2> History </h2>
+ * <p>It uses the old Cryptix name in a vain hope to restart that
+ * project after a decade of silence.</p>
+ * 
+ * <p>May your packets be secure.  Hack on!</p>
+ * 
  * @author ada
+ * @author iang
  *
  */
 package cryptix;
